8c24cffac9d3f6ea1ffd932bbd4583a170736de8
ValGibello <32450304+ValGibello@users.noreply.github.com>
2018-03-07 09:55:55 +0100

    Version 1.0

--- main.py
+++ main.py
@@ -1,54 +1,51 @@
-### MODULE 1 - DOWNLOADING HTML ###
-
 from lxml import html
 import requests
 import re
 import datetime
 import telegram_send
 
+### Récupération du JO ###
+
 adresse_jo=["https://www.legifrance.gouv.fr/affichJO.do?idJO="]
 jour = datetime.datetime.now()
 
 print("Nous sommes le " + (jour.strftime("%d/%m/%Y")))
 
-#from pprint import pprint
-
 print("Connection à Légifrance" + "\n" + "Téléchargement du JO..." + "\n")
 
 page = requests.get('https://www.legifrance.gouv.fr/affichJO.do?idJO=')
 contenu = html.fromstring(page.content)
-
 liens_sommaire = contenu.xpath('//a[@class="lienSommaire"]/text()')
-### print(liens_sommaire)
 
-nb_lignes = str(len(liens_sommaire))
+nb_entrées = str(len(liens_sommaire))
 
-print("Il y a " + nb_lignes + " entrées dans le JO du jour." + "\n")
+print("Il y a " + nb_entrées + " entrées dans le JO du jour." + "\n")
 print("Détection..." + "\n")
 
-### MODULE 2 - PARSING ###
-
-keywords = ['traitement', 'données']
-keywords_re = re.compile("|".join(keywords))
+### PARSING ###
 
 nb_traitements = int()
 contenu_utile = []
+
+keywords = ["création d'un traitement", "données"]
+keywords_re = re.compile("|".join(keywords))
 
 for item in liens_sommaire:
     if keywords_re.search(str(item)):
         nb_traitements += 1
         contenu_utile.append(item)
-        print("Un traitement publié au JO." + "\n")
+        print("Un traitement intéressant publié au JO." + "\n")
     
+### GENERATION D'UN RAPPORT ET DIFFUSION ###
 
-print("Il y a " + str(nb_traitements) + " entrée(s) intéressantes." + "\n" + "************************************************************" + "\n" + "\n")
+if len(contenu_utile)>0:
+    print("Il y a " + str(nb_traitements) + " entrée(s) intéressante(s)." + "\n" + "************************************************************" + "\n" + "\n")
+    print("************************************************************" + "\n" + "Génération d'un rapport..." + "\n" + "************************************************************" + "\n")
+    print(*contenu_utile, sep="\n \n")
+    telegram_send.send(["Bonjour ! Voici la sélection Journal Officiel du jour."])
+    telegram_send.send(messages=contenu_utile)
+    telegram_send.send(messages=adresse_jo, parse_mode="text")
+else:
+    print("Aucun traitement intéressant publié au JO. Essayez de modifier les règles de tri.")
 
-### MODULE 3 - CREATING REPORT ###
-print("************************************************************" + "\n" + "Génération d'un rapport..." + "\n" + "************************************************************" + "\n")
-
-
-print(*contenu_utile, sep="\n \n")
-telegram_send.send(["Bonjour ! Voici la sélection Journal Officiel du jour."])
-telegram_send.send(messages=contenu_utile)
-telegram_send.send(messages=adresse_jo, parse_mode="text")
 
